1df2f9fa8669db06be5bb2f4e985b1cdc51af2f7
Lok3rs <lok3rsss@gmail.com>
2022-04-08 16:55:26 +0200

    Change strawberry_django library to strawberry_django_plus

--- api/company/schema.py
+++ api/company/schema.py
@@ -1,25 +1,33 @@
 import strawberry
+from strawberry_django_plus import gql
+from strawberry_django_plus.optimizer import DjangoOptimizerExtension
 from typing import List
 
 from company import types, models
 
 
-@strawberry.type
+@gql.django.input(models.Company)
+class CompanyInput:
+    name: gql.auto
+
+
+@gql.django.partial(models.Company)
+class CompanyInputPartial(gql.NodeInput):
+    name: gql.auto
+
+
+@gql.type
 class Query:
-    company: types.Company = strawberry.django.field()
-    employee: types.Employee = strawberry.django.field()
-    project: types.Project = strawberry.django.field()
-    companies: List[types.Company] = strawberry.django.field()
-    employees: List[types.Employee] = strawberry.django.field()
-    projects: List[types.Project] = strawberry.django.field()
+    companies: List[types.Company] = gql.django.field()
+    employees: List[types.Employee] = gql.django.field()
+    projects: List[types.Project] = gql.django.field()
 
 
-@strawberry.type
+@gql.type
 class Mutation:
+    create_company: types.Company = gql.django.create_mutation(CompanyInput)
+    update_company: types.Company = gql.django.update_mutation(CompanyInputPartial)
+    delete_company: types.Company = gql.django.delete_mutation(gql.NodeInput)
 
-    @strawberry.mutation
-    def add_company(self, name: str) -> types.Company:
-        return types.Company(name=name, employees=[])
 
-
-schema = strawberry.Schema(query=Query, mutation=Mutation)
+schema = strawberry.Schema(query=Query, mutation=Mutation, extensions=[DjangoOptimizerExtension])
--- api/company/types.py
+++ api/company/types.py
@@ -1,24 +1,26 @@
-import strawberry
-from strawberry.django import auto
+from strawberry_django_plus import gql
 from typing import List
 from company import models
 
 
-@strawberry.django.type(models.Company)
+@gql.django.type(models.Company)
 class Company:
+    id: gql.auto
     name: str
     employees: List['Employee']
 
 
-@strawberry.django.type(models.Employee)
+@gql.django.type(models.Employee)
 class Employee:
+    id: gql.auto
     first_name: str
     last_name: str
     company: Company
     projects: List['Project']
 
 
-@strawberry.django.type(models.Project)
+@gql.django.type(models.Project)
 class Project:
+    id: gql.auto
     duration_hours: float
     employees: List[Employee]
