0809785c8c0908417689eecb2b62a1d202768458
Lok3rs <lok3rsss@gmail.com>
2022-04-08 14:10:25 +0200

    First mutation to add new company

--- api/company/schema.py
+++ api/company/schema.py
@@ -1,14 +1,25 @@
 import strawberry
 from typing import List
 
-from company import types
+from company import types, models
 
 
 @strawberry.type
 class Query:
+    company: types.Company = strawberry.django.field()
+    employee: types.Employee = strawberry.django.field()
+    project: types.Project = strawberry.django.field()
     companies: List[types.Company] = strawberry.django.field()
     employees: List[types.Employee] = strawberry.django.field()
     projects: List[types.Project] = strawberry.django.field()
 
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+
+    @strawberry.mutation
+    def add_company(self, name: str) -> types.Company:
+        return types.Company(name=name, employees=[])
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
--- api/company/types.py
+++ api/company/types.py
@@ -6,14 +6,12 @@ from company import models
 
 @strawberry.django.type(models.Company)
 class Company:
-    id: auto
     name: str
     employees: List['Employee']
 
 
 @strawberry.django.type(models.Employee)
 class Employee:
-    id: auto
     first_name: str
     last_name: str
     company: Company
@@ -22,6 +20,5 @@ class Employee:
 
 @strawberry.django.type(models.Project)
 class Project:
-    id: auto
     duration_hours: float
     employees: List[Employee]
